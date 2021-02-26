a33fee6e21906334129c2245fb98e11d1a692a8b
Greg Brosman <greg.brosman@gmail.com>
2021-02-25 18:19:32 -0800

    Particles background added. Clarifai API added.

--- src/App.js
+++ src/App.js
@@ -3,18 +3,38 @@ import Navigation from './components/Navigation/navigation';
 import Logo from './components/Logo/logo';
 import Rank from './components/Rank/rank';
 import ImageLinkForm from './components/ImageLinkForm/imagelinkform';
+import Particles from 'react-particles-js';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <Navigation />
-      <Logo />
-      <Rank />
-      <ImageLinkForm />
-      {/*<FaceRecognition />*/}
-    </div>
-  );
+
+// Params for the particle background. Located here to keep the return block cleaner.
+const particlesOptions = {
+  particles: {
+    number: {
+      value: 150, 
+      density: {
+        enable: true,
+        value_area: 800
+      }
+    }
+  }
 }
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Particles className='particles' params={ particlesOptions } />
+        <Navigation />
+        <Logo />
+        <Rank />
+        <ImageLinkForm />      
+        {/*<FaceRecognition />*/}
+      </div>
+    );
+  }
+}
+
 
 export default App;
