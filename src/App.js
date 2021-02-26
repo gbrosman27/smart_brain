69fdaf3dd11ca0c7b5a0c54393322699b2d85aec
Greg Brosman <greg.brosman@gmail.com>
2021-02-25 18:58:32 -0800

    onInputChange, onButtonSubmit functionality.

--- src/App.js
+++ src/App.js
@@ -22,6 +22,23 @@ const particlesOptions = {
 
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      input: '',
+    }
+  }
+
+  // Input to the imagelinkform.
+  onInputChange = (event) => {
+    console.log(event.target.value);
+  }
+
+  onButtonSubmit = () => {
+    console.log('click');
+  }
+
+
   render() {
     return (
       <div className="App">
@@ -29,7 +46,7 @@ class App extends Component {
         <Navigation />
         <Logo />
         <Rank />
-        <ImageLinkForm />      
+        <ImageLinkForm onInputChange={ this.onInputChange } onButtonSubmit={ this.onButtonSubmit } />      
         {/*<FaceRecognition />*/}
       </div>
     );
