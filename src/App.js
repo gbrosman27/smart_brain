4a2d1c87a5fa0c69fc97ba4483be51968eda358b
Greg Brosman <greg.brosman@gmail.com>
2021-03-20 11:53:54 -0700

    navigation functionality. nav words change as screens change.

--- src/App.js
+++ src/App.js
@@ -38,7 +38,8 @@ class App extends Component {
       input: '',
       imageUrl: '',
       box: {},
-      route: 'signin'
+      route: 'signin',
+      isSignedIn: false
     }
   }
 
@@ -79,6 +80,11 @@ class App extends Component {
 
   // Route changes between home, sign in, register, and sign out.
   onRouteChange = (route) => {
+    if (route === 'signout'){
+      this.setState({isSignedIn: false});
+    } else if (route === 'home'){
+      this.setState({isSignedIn: true})
+    }
     this.setState({route: route});
   }
 
@@ -86,7 +92,7 @@ class App extends Component {
     return (
       <div className="App">
         <Particles className='particles' params={ particlesOptions } />
-        <Navigation onRouteChange={this.onRouteChange} />
+        <Navigation isSignedIn={this.state.isSignedIn} onRouteChange={this.onRouteChange} />
         {/* if this.state.route === signin is true, return signin component, else return everything in the div */}
         { this.state.route === 'home' ?
           <div>
--- src/components/Navigation/navigation.js
+++ src/components/Navigation/navigation.js
@@ -1,12 +1,24 @@
 import React from 'react';
 
-const Navigation = ({ onRouteChange }) => {
-    return(
-    <nav style={{display: 'flex', justifyContent: 'flex-end'}}>
-        {/* Styled using tachyons. Imported to index.js */}
-        <p onClick={() => onRouteChange('signin')} className='f3 link dim black underline pa3 pointer'>Sign Out</p> 
-    </nav>
-    )
+
+// Change navigation based on if user is signed in or not.
+const Navigation = ({ onRouteChange, isSignedIn }) => {
+        if(isSignedIn){
+            return(
+            <nav style={{display: 'flex', justifyContent: 'flex-end'}}>
+            {/* Styled using tachyons. Imported to index.js */}
+            <p onClick={() => onRouteChange('signout')} className='f3 link dim black underline pa3 pointer'>Sign Out</p> 
+            </nav>)
+        } 
+        else{
+            return(
+                <nav style={{display: 'flex', justifyContent: 'flex-end'}}>
+                <p onClick={() => onRouteChange('signin')} className='f3 link dim black underline pa3 pointer'>Sign In</p>
+                <p onClick={() => onRouteChange('register')} className='f3 link dim black underline pa3 pointer'>Register</p> 
+                </nav>)
+        }
+
 }
+    
 
 export default Navigation;
